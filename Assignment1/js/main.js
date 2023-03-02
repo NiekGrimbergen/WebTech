41b1659e3237841480a6f391c1791dca489ae612
NiekGrimbergen <67412429+NiekGrimbergen@users.noreply.github.com>
2023-03-02 14:53:00 +0100

    refactoring

--- Assignment1/js/main.js
+++ Assignment1/js/main.js
@@ -1,34 +1,58 @@
 import {mattDamon} from './data.js';
 
 
-const main = document.querySelector('main');
-const article = document.createElement('article');
-main.className = 'content';
-main.appendChild(article);
-const h1 = document.createElement('h1');
-h1.textContent = 'info';
-h1.className = 'title';
-article.appendChild(h1);
 
-function addArtist(article, artist) {
+
+//creates an article with a title
+function createArticle(parent, title) {
+    var article = document.createElement('article');
+    var h1 = document.createElement('h1');
+
+    h1.textContent = 'info';
+    h1.className = 'title';
+    article.appendChild(h1);
+    parent.appendChild(article);
+    return article;
+}
+
+//creates a section following the template
+function newSection(article) {
     var section = document.createElement('section');
     section.className = 'section';
     article.appendChild(section);
+    return section;
+}
 
+//creates a new section header for a specified section
+function newSectionHeader(section, content) {
     var header = document.createElement('h2');
     header.className = 'section__title';
-    header.textContent = artist.name;
+    header.textContent = content;
     section.appendChild(header);
+}
 
-    var movies = document.createElement('ul');
-    movies.className = 'actor__movies';
-    section.appendChild(movies);
+function newMovieList(section, movies) {
+    var list = document.createElement('ul');
+    list.className = 'actor__movies';
+    section.appendChild(list);
 
-    for (var i = 0; i < artist.movies.length; i++) {
+    for (var i = 0; i < movies.length; i++) {
         let movie = document.createElement('li');
-        movie.textContent = artist.movies[i];
-        movies.appendChild(movie);
+        movie.textContent = movies[i];
+        list.appendChild(movie);
     }
 }
 
-addArtist(article, mattDamon);
+//creates a section about an actor inside a specified article
+function addActor(article, actor) {
+    var section = newSection(article);
+    newSectionHeader(section, actor.name);
+    newMovieList(section, actor.movies);
+}
+
+const main = document.querySelector('main');
+main.className = 'content';
+
+const info = createArticle(main, 'info');
+
+addActor(info, mattDamon);
