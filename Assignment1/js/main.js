edba710ca5674c075d3742eaf441a84ee1f9da0c
NiekGrimbergen <67412429+NiekGrimbergen@users.noreply.github.com>
2023-03-03 16:59:56 +0100

    aanpassingun

--- Assignment1/js/main.js
+++ Assignment1/js/main.js
@@ -3,10 +3,10 @@ import {mattDamon, jessicaChastain, kristenWiig, chiwetelEjiofor,
     ridleyScott, andyWeir} from './data.js';
 
 //creates an article with a title
-function createArticle(parent, title) {
+function newArticle(parent, title) {
     var article = document.createElement('article');
+    article.id = title;
     var h1 = document.createElement('h1');
-
     h1.textContent = 'info';
     h1.className = 'title';
     article.appendChild(h1);
@@ -17,57 +17,63 @@ function createArticle(parent, title) {
 //creates a section following the template
 function newSection(article) {
     var section = document.createElement('section');
-    section.className = 'section';
+    section.classList.add('artist');
     article.appendChild(section);
     return section;
 }
 
 //creates a new section header for a specified section
-function newSectionHeader(section, content) {
+function sectionHeader(section, content) {
     var header = document.createElement('h2');
-    header.className = 'section__title';
+    header.className = 'artist__name';
     header.textContent = content;
     section.appendChild(header);
 }
 
-//creates a <ul> containing an artists work
-function newTitlesList(section, titles, type) {
-    //add a header to the list
-    var typeOfWork = document.createElement('h3');
-    typeOfWork.textContent = type + ":";
-    section.appendChild(typeOfWork);
-    //create the list itself
-    var list = document.createElement('ul');
-    list.className = 'actor__movies';
-    section.appendChild(list);
-    //add the titles to the ul as li's
-    for (var i = 0; i < titles.length; i++) {
-        let movie = document.createElement('li');
-        movie.textContent = titles[i];
-        list.appendChild(movie);
-    }
+// creates an figure with img with alt attribute, which is the name of the artist
+function artistPhoto(section, photo, alt) {
+    var fig = document.createElement('figure');
+    fig.className = 'artist__figure';
+    section.appendChild(fig);
+
+    var img = document.createElement('img');
+    img.className = 'artist__img';
+    img.src = photo;
+    img.alt = alt;
+    fig.appendChild(img);
 }
 
-function actorPhoto(section, photo){
-    var img = document.createElement('img');
-    img.src = photo;
-    section.appendChild(img);
+function ArtistParTitle(section, text) {
+    var title = document.createElement('h3');
+    title.className = 'artist__h3';
+    title.textContent = text + ":";
+    section.appendChild(title);
+}
+
+function ArtistPar(section, text) {
+    var par = document.createElement('p');
+    par.textContent = text;
+    section.appendChild(par);
+}
+
+function artistSubsection(section, title, content) {
+    ArtistParTitle(section, title);
+    ArtistPar(section, content);
 }
 
 //creates a section about an artist inside a specified article
 function newArtistSection(article, artist) {
     var section = newSection(article);
-    newSectionHeader(section, artist.name);
+    artistPhoto(section, artist.photo, artist.name);
+    sectionHeader(section, artist.name);
     //some artists have movies, some have books, let's add them
-    if (artist.photo){
-        actorPhoto(section, artist.photo);
-    }
     if (artist.movies) {
-        newTitlesList(section, artist.movies, "Movies");
+        artistSubsection(section, "Movies", artist.movies.join(', '));
     }
     if (artist.books) {
-        newTitlesList(section, artist.books, "Books");
+        artistSubsection(section, "Books", artist.books.join(', '));
     }
+    artistSubsection(section, "Bio", artist.bio);
 }
 
 //set the classname of main to content for our css
@@ -75,7 +81,7 @@ const main = document.querySelector('main');
 main.className = 'content';
 
 //the assignment requires to build articles with js:
-const info = createArticle(main, 'info');
+const info = newArticle(main, 'info');
 
 //let's add some artists to our info article
 newArtistSection(info, mattDamon);
